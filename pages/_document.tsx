a103069dd29e69a126d943ed03415558bc517d90
Even Østerud <evenost@ulrik.uio.no>
2026-01-04 22:40:15 +0100

    Add favicon, dagen @

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/web-design/dagen-logo/dagen_at.svg" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />

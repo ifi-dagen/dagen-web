0092ba8920ceb52064aea1198b346111a2b17550
Even Østerud <evenost@ulrik.uio.no>
2026-01-17 00:17:26 +0100

    Update bli-med from Figma design - started with overlay

--- pages/bli-med.tsx
+++ pages/bli-med.tsx
@@ -2,24 +2,75 @@ import ReactMarkdown from "react-markdown";
 import ContentRowBuilder from "@/components/ContentRowBuilder";
 import { getContentRowLayout } from "@/lib/getContentRowLayout";
 import { getInternGroups } from "../lib/getInternGroups";
-import { JoinUsProps } from "@/types";
+import { InternGroup, JoinUsProps } from "@/types";
+import JoinUsCard from "@/components/JoinUsCard";
+import { getMarkdownContent } from "@/lib/getMarkdownContent";
 
-export default function Page({ contentRows, internGroups }: JoinUsProps) {
+type JoinProps = {
+  internGroups: InternGroup[];
+  bliMedInfo: string;
+  funkInfo: string;
+  funkExtended: string;
+  internInfo: string;
+  internExtended: string;
+  styretInfo: string;
+  styretExtended: string;
+}
+
+export default function Page({
+  internGroups,
+  bliMedInfo,
+  funkInfo,
+  funkExtended,
+  internInfo,
+  internExtended,
+  styretInfo,
+  styretExtended,
+}: JoinProps) {
   return (
-    <main className="max-w-7xl mx-auto px-4 md:px-6 py-8 space-y-12">
-      
-      {/* Bygger alt innhold fra bli_med.csv */}
-      <ContentRowBuilder rows={contentRows} />
-      
-      {/* Interngrupper */}
-      <div className="space-y-4">
-        <h2 className="text-4xl font-bold text-(--primary) text-center">Intergruppene</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {internGroups.map((group, index) => (
-            <div key={index} className="prose p-6">
-              <ReactMarkdown>{group.content}</ReactMarkdown>
-            </div>
-          ))}
+    <main className="max-w-[1304px] mx-auto mt-[260px]">
+      <div
+        className="max-w-[1107px] mx-auto">
+        <p
+          className={[
+            "text-black text-lg font-normal font-mono leading-8",
+            "tracking-wide text-justify",
+          ].join(" ")}
+        >
+          {bliMedInfo}
+        </p>
+      </div>
+
+      <div className="flex flex-row justify-center gap-[63px] py-[114px]">
+        {/* Funk */}
+        <div>
+          <JoinUsCard
+            title="Funksjonær;"
+            infoText={funkInfo}
+            popupLink="null"
+            applyLink="null"
+          />
+        </div>
+
+        {/* Intern */}
+        <div>
+          <JoinUsCard
+            title="Intern;"
+            infoText={internInfo}
+            popupLink="null"
+            applyLink="null"
+          />
+        </div>
+
+
+        {/* Styret */}
+        <div>
+          <JoinUsCard
+            title="Styremedlem;"
+            infoText={styretInfo}
+            popupLink="null"
+            applyLink="null"
+          />
         </div>
       </div>
     </main>
@@ -27,10 +78,24 @@ export default function Page({ contentRows, internGroups }: JoinUsProps) {
 }
 
 export function getStaticProps() {
-  const contentRows = getContentRowLayout("bli-med/bli_med.csv");
   const internGroups = getInternGroups();
-  return { props: { 
-    contentRows,
-    internGroups,
-  } };
+  const bliMedInfo = getMarkdownContent("bli-med/bli_med");
+  const funkInfo = getMarkdownContent("bli-med/funk_info");
+  const funkExtended = getMarkdownContent("bli-med/funk_extended");
+  const internInfo = getMarkdownContent("bli-med/intern_info");
+  const internExtended = getMarkdownContent("bli-med/intern_extended");
+  const styretInfo = getMarkdownContent("bli-med/styret_info");
+  const styretExtended = getMarkdownContent("bli-med/styret_extended");
+  return {
+    props: {
+      internGroups,
+      bliMedInfo,
+      funkInfo,
+      funkExtended,
+      internInfo,
+      internExtended,
+      styretInfo,
+      styretExtended,
+    }
+  };
 }

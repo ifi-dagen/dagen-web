21e44667ee96989cdd9b9ba71c822231b56e6e35
Yrjar Vederhus <yrjar@yrjar.tech>
2026-01-26 16:12:40 +0100

    Add prose to ReactMarkdown parent divs

--- pages/bli-med.tsx
+++ pages/bli-med.tsx
@@ -138,7 +138,7 @@ export default function JoinUsPage({
           {/* Infotekst */}
           <div className="pb-20">
             <div className="text-justify text-black text-lg font-normal font-mono 
-                            leading-8 tracking-wide whitespace-pre-line">
+                            leading-8 tracking-wide whitespace-pre-line prose">
               <ReactMarkdown>{overlayData?.body ?? ""}</ReactMarkdown>
             </div>
 
